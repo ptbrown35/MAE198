66701714cbb1e5d90639092286c8ad6e5fdcce60
Parker Brown <pthb35@gmail.com>
2017-11-24 21:20:56 -0800

    read tweaks

--- urm07/test/urm07_read.py
+++ urm07/test/urm07_read.py
@@ -4,33 +4,36 @@ import time
 
 sample_time = 0.05
 
+# Command Frame Header
 header_H = 0x55 #Header
 header_L = 0xAA #Header
 
-
-device_Addr = 0xAB #Address
-#device_Addr = 0x22 #Address
-#device_Addr = 0x33 #Address
-#device_Addr = 0x44 #Address
+# Device Addresses
+device_Addr = 0xAB #Address: Generic
+#device_Addr = 0x22 #Address: 0x22
+#device_Addr = 0x33 #Address: 0x33
+#device_Addr = 0x44 #Address: 0x44
 
 data_Length = 0x00 #Data length
 get_Dis_CMD = 0x02 #Command: Read Distance
 get_Temp_CMD = 0x03 #Command: Read Temperature
 
-checksum_d = 0xAC #Distance checksum
-#checksum_d = 0x23 #Distance checksum
-#checksum_d = 0x34 #Distance checksum
-#checksum_d = 0x45 #Distance checksum
+checksum_d = 0xAC #Distance checksum: 0xAB
+#checksum_d = 0x23 #Distance checksum: 0x22
+#checksum_d = 0x34 #Distance checksum: 0x33
+#checksum_d = 0x45 #Distance checksum: 0x44
 
-checksum_t = 0xAD #Temperature checksum
-#checksum_t = 0x24 #Temperature checksum
-#checksum_t = 0x35 #Temperature checksum
-#checksum_t = 0x46 #Temperature checksum
+checksum_t = 0xAD #Temperature checksum: 0xAB
+#checksum_t = 0x24 #Temperature checksum: 0x22
+#checksum_t = 0x35 #Temperature checksum: 0x33
+#checksum_t = 0x46 #Temperature checksum: 0x44
 
+# Distance Command Frame
 cmd_d = bytearray([header_H, header_L, device_Addr, data_Length, get_Dis_CMD, checksum_d])
-
+# Temperature Command Frame
 cmd_t = bytearray([header_H, header_L, device_Addr, data_Length, get_Temp_CMD, checksum_t])
 
+# Serial Port Configuration
 ser = serial.Serial()
 ser.port = '/dev/ttyS0'
 ser.baudrate = 19200
@@ -39,12 +42,17 @@ ser.partiy = 'N'
 ser.stopbits = 1
 ser.timeout = 5
 #ser.write_timeout = 2
+# Output Serial Port Configuration
 print('Serial port data: ')
 print(ser)
 print()
 
-ser.open()
-print('Serial port open: ' + str(ser.is_open))
+
+if ser.open():
+    print('Serial port open: ' + str(ser.is_open))
+else:
+    print('Failed to open Serial Port. Exiting...')
+    exit()
 
 print('Distance (cm) | Temperature (C)')
 c = 0
